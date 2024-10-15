6e4a201e3fd4392e2ca26a682543601ed8bfdf8d
Timon Viola <viotimo@dfds.com>
2024-10-15 18:16:47 +0200

    feat: add SQLReflectOperator to operators interface

--- src/dagcellent/operators/__init__.py
+++ src/dagcellent/operators/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from dagcellent.operators.external_table_arrow import CreateExternalTableArrow
+from dagcellent.operators.sql_reflect import SQLReflectOperator
 from dagcellent.operators.sql_s3 import SqlToS3Operator
 
-__all__ = ["SqlToS3Operator", "CreateExternalTableArrow"]
+__all__ = ["SQLReflectOperator", "SqlToS3Operator", "CreateExternalTableArrow"]
